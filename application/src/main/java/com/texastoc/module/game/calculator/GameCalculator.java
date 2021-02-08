1b67d8d5eb9a42ef6a1546f3b1897d1db7857aee
Gil Pratte <gpratte@users.noreply.github.com>
2021-02-08 16:24:04 -0600

    26 game calculation integration tests
    * plumbing in place for game calulations integration testing
    
    * first game calculations integration test - game with no players
    
    * pass list of players to add to the game
    
    * test calculations for a game with one player loaded
    
    * test calculations for a game with 10 players
    
    * fleshed out test calculations for a game with 10 players
    
    * test calculations when updating a game player
    
    * test calculations when adding and deleting game players
    
    * update readme

--- application/src/main/java/com/texastoc/module/game/calculator/GameCalculator.java
+++ application/src/main/java/com/texastoc/module/game/calculator/GameCalculator.java
@@ -18,6 +18,7 @@ public class GameCalculator {
 
   public Game calculate(Game game) {
     int numPlayers = 0;
+    int numPaidPlayers = 0;
     int buyInCollected = 0;
     int rebuyAddOnCollected = 0;
     int annualTocCollected = 0;
@@ -25,17 +26,23 @@ public class GameCalculator {
     int totalCollected = 0;
     int kittyCalculated = 0;
     int annualTocFromRebuyAddOnCalculated = 0;
+    boolean chopped = false;
 
     if (game.getPlayers() != null) {
       for (GamePlayer gamePlayer : game.getPlayers()) {
         ++numPlayers;
-        buyInCollected += gamePlayer.isBoughtIn() ? game.getBuyInCost() : 0;
+        if (gamePlayer.isBoughtIn()) {
+          buyInCollected += game.getBuyInCost();
+          ++numPaidPlayers;
+        }
         rebuyAddOnCollected += gamePlayer.isRebought() ? game.getRebuyAddOnCost() : 0;
         annualTocCollected += gamePlayer.isAnnualTocParticipant() ? game.getAnnualTocCost() : 0;
         quarterlyTocCollected += gamePlayer.isQuarterlyTocParticipant() ? game.getQuarterlyTocCost() : 0;
-
         if (gamePlayer.isAnnualTocParticipant() && gamePlayer.isRebought()) {
           annualTocFromRebuyAddOnCalculated += game.getRebuyAddOnTocDebitCost();
+        }
+        if (gamePlayer.getChop() != null) {
+          chopped = true;
         }
       }
     }
@@ -45,11 +52,13 @@ public class GameCalculator {
     }
 
     game.setNumPlayers(numPlayers);
+    game.setNumPaidPlayers(numPaidPlayers);
     game.setBuyInCollected(buyInCollected);
     game.setRebuyAddOnCollected(rebuyAddOnCollected);
     game.setAnnualTocCollected(annualTocCollected);
     game.setQuarterlyTocCollected(quarterlyTocCollected);
-
+    game.setChopped(chopped);
+  
     totalCollected += buyInCollected + rebuyAddOnCollected + annualTocCollected + quarterlyTocCollected;
     game.setTotalCollected(totalCollected);
     game.setKittyCalculated(kittyCalculated);
